115350c8bd83c67c6fb0102a649e1e4cb8813841
JWest2323 <jwest23@ufl.edu>
2023-03-11 23:24:19 -0600

    update WhatGPT3 in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Footer, Blog, Possibility, Features, whatGPT3, Header } from './containers';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components'
 import './App.css'
 
@@ -12,7 +12,7 @@ const App = () => {
         <Header />
       </div>
       <Brand />
-      <whatGPT3 />
+      <WhatGPT3 />
       <Features />
       <Possibility />
       <CTA />
